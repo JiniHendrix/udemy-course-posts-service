951d0baf5b329d4c05dad7d062423a4103ee5bd8
Jin Choi <jinchoi@ucdavis.edu>
2020-07-09 16:43:27 -0700

    added read and create route, storing in memory

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const { randomBytes } = require('crypto')
+const app = express()
+
+const posts = {}
+
+app.use(express.json())
+
+app.get('/posts', (req, res) => {
+  res.send(posts)
+})
+
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex')
+  const { title } = req.body
+
+  posts[id] = {
+    id, title
+  }
+
+  res.status(201).send(posts[id])
+})
+
+app.listen(3001, () => { console.log('Posts server listening on 3001')}
